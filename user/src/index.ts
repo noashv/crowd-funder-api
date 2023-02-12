2c7521f5ae8062d47ca534fce42365ed4efc5645
Noa Shvartzman <noa@equitybee.com>
2023-02-12 12:51:49 +0200

    configure bunyan logger

--- user/src/index.ts
+++ user/src/index.ts
@@ -3,12 +3,13 @@ require('module-alias/register');
 import 'reflect-metadata';
 import express from 'express';
 import configApollo from '@/config/apollo';
+import log from '@/config/logger';
 
 const main = async () => {
   const app = express();
   configApollo(app);
 
-  app.listen(4000, () => { console.log('server started on http://localhost:4000/graphql'); });
+  app.listen(4000, () => { log.info('server started on http://localhost:4000/graphql'); });
 };
 
 main();
