96550543c3729625ebfb7e7933b9552b7197d395
Noa Shvartzman <noa@equitybee.com>
2023-02-12 20:40:02 +0200

    configed typeorm client

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,13 +1,20 @@
+import * as dotenv from 'dotenv';
+
 require('module-alias/register');
 
 import 'reflect-metadata';
 import express from 'express';
 import configApollo from '@/config/apollo';
+import connectToDB from '@/config/typeorm';
 import log from '@/config/logger';
 
+dotenv.config();
+
+console.log('hewwow', process.env.POSTGRES_USER);
 const main = async () => {
   const app = express();
   configApollo(app);
+  connectToDB();
 
   app.listen(4000, () => { log.info('server started on http://localhost:4000/graphql'); });
 };
