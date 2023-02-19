88df995bbb15fe3ca84b45cc8fc2f281f4327135
noashv <noashv2501@gmail.com>
2023-02-19 19:58:44 +0200

    some fixes

--- user/src/config/apollo/index.ts
+++ user/src/config/apollo/index.ts
@@ -3,7 +3,7 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import { json } from 'body-parser';
-import { createSchema } from '@/modules';
+import createSchema from '@/modules/modules.schema';
 import formatError from './format-error';
 
 const configApollo = async (app) => {
